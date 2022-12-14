3bfa862b932eb003559879dd60354c05ae916c65
Rahulaher0080 <rahulaher0008@gmail.com>
2022-12-14 18:28:31 +0530

    ADDED

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+CSV_FILE_PATH = os.path.join(os.getcwd(),r"CSV_file\out.csv")
+
+MODEL_FILE_PATH = os.path.join(os.getcwd(),r"artifacts\linear_reg.pkl")
+
+PROJECT_FILE_PATH = os.path.join(os.getcwd(),r"artifacts\project_data.json")
+
+PORT_NUMBER = 5004
---
+++ interface.py
@@ -0,0 +1,35 @@
+from flask import Flask , jsonify,render_template,request,redirect,url_for
+from utils import CarPrice
+import config
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_flask():
+    print("Welcome to Car price prediction")
+    return render_template("index.html")
+
+@app.route("/car_price",methods = ["GET","POST"])
+
+def prediction():
+    
+    # if request.method == "POST":
+    #     print("We are in Post method")
+    
+    data = request.form
+    print("Data :",data)
+
+    year     = int(request.form["year"])
+    mileage  = int(request.form["mileage"])
+    hp       = int(request.form["hp"])
+    gear     = request.form["gear"]
+    fuel     = request.form["fuel"]
+    make     = request.form["make"]
+
+    car_price = CarPrice(data)
+    price = car_price.get_pred_price()
+
+    return jsonify({"Price": price})
+    return render_template("index.html",prediction = price)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port=config.PORT_NUMBER)
